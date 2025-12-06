7678161a43c96a4929db53993885f13fb7db657a
MASUTANI Yasuhiro <masutani@isc.osakac.ac.jp>
2025-12-06 19:03:38 +0900

    new_bringme_action_server_node.pyを英訳

--- airobot_action/airobot_action/new_bringme_action_server_node.py
+++ airobot_action/airobot_action/new_bringme_action_server_node.py
@@ -5,15 +5,15 @@ from rclpy.node import Node
 from rclpy.action import ActionServer, CancelResponse
 from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.executors import MultiThreadedExecutor
-from airobot_interfaces.action import StringCommand  # カスタムアクション定義のインポート
+from airobot_interfaces.action import StringCommand  # Import custom action definition
 
 
 class BringmeActionServer(Node):
     def __init__(self):
         super().__init__('bringme_action_server')
-        self.goal_handle = None    # 処理中のゴールの情報を保持する変数
-        self.goal_lock = Lock()    # 二重実行させないためのロック変数
-        self.execute_lock = Lock() # 二重実行させないためのロック変数
+        self.goal_handle = None    # Variable to store the active goal info
+        self.goal_lock = Lock()    # Lock to avoid double execution
+        self.execute_lock = Lock() # Lock to avoid double execution
         self._action_server = ActionServer(
             self, StringCommand, 'command', 
             execute_callback=self.execute_callback,
@@ -24,30 +24,30 @@ class BringmeActionServer(Node):
         self.food = ['apple', 'banana', 'candy']
 
     def handle_accepted_callback(self, goal_handle):
-        with self.goal_lock:                # ブロック内を二重実行させない
+        with self.goal_lock:                # Avoid double execution in this block
             if self.goal_handle is not None and self.goal_handle.is_active:
-                self.get_logger().info('前の処理を中断')
+                self.get_logger().info('Abort previous process')
                 self.goal_handle.abort()
-            self.goal_handle = goal_handle  # ゴール情報の更新
-        goal_handle.execute()               # ゴール処理の実行
+            self.goal_handle = goal_handle  # Update goal info
+        goal_handle.execute()               # Execute goal
 
     def execute_callback(self, goal_handle):
-        with self.execute_lock:            # ブロック内を二重実行させない
+        with self.execute_lock:            # Avoid double execution in this block
             feedback = StringCommand.Feedback()
             result = StringCommand.Result()
             count = random.randint(5, 10)
 
             while count > 0:
                 if not goal_handle.is_active:
-                    self.get_logger().info('中断処理')
+                    self.get_logger().info('Abort process')
                     return result
 
                 if goal_handle.is_cancel_requested:
-                    self.get_logger().info('キャンセル処理')
+                    self.get_logger().info('Cancel process')
                     goal_handle.canceled()
                     return result
 
-                self.get_logger().info(f'フィードバック送信中: 残り{count}[s]')     
+                self.get_logger().info(f'Sending feedback: {count}[s] left')     
                 feedback.process = f'{count}'
                 goal_handle.publish_feedback(feedback)  
                 count -= 1  
@@ -55,25 +55,25 @@ class BringmeActionServer(Node):
 
             item = goal_handle.request.command
             if item in self.food:
-                result.answer =f'はい，{item}です'
+                result.answer =f'Yes, here is {item}'
             else:
-                result.answer = f'{item}を見つけることができませんでした'
+                result.answer = f'Could not find {item}'
             goal_handle.succeed()
-            self.get_logger().info(f'ゴールの結果: {result.answer}')
+            self.get_logger().info(f'Goal result: {result.answer}')
             return result
 
     def cancel_callback(self, goal_handle):
-        self.get_logger().info('キャンセル受信')
+        self.get_logger().info('Cancel received')
         return CancelResponse.ACCEPT
 
 
 def main():
     rclpy.init()
     bringme_action_server = BringmeActionServer()
-    print('サーバ開始')
+    print('Server started')
     try:
         rclpy.spin(bringme_action_server, executor=MultiThreadedExecutor())
     except KeyboardInterrupt:
         pass
     rclpy.try_shutdown()
-    print('サーバ終了')
+    print('Server stopped')
